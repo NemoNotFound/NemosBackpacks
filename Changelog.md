dea76fc6a3f686fb590ada6dc4f2bf5455081606
NemoNotFound <02grimes-bard@icloud.com>
2025-10-31 20:54:54 +0100

    Update 1.2.1

--- Changelog.md
+++ Changelog.md
@@ -1,4 +1,7 @@
 # Changelog v1.2.1
 
+## Additions
+- Added Ukranian translation
+
 ## Fixes
 - Fixed an issue where swapping an item with the off-hand in the backpack screen didn’t update correctly on screen.
--- fabric/src/main/java/com/nemonotfound/nemos/backpacks/DataGenerator.java
+++ fabric/src/main/java/com/nemonotfound/nemos/backpacks/DataGenerator.java
@@ -4,6 +4,7 @@ import com.nemonotfound.nemos.backpacks.datagen.BackpackRecipeProvider;
 import com.nemonotfound.nemos.backpacks.datagen.ItemTagProvider;
 import com.nemonotfound.nemos.backpacks.datagen.ModelProvider;
 import com.nemonotfound.nemos.backpacks.datagen.lang.EnglishLanguageProvider;
+import com.nemonotfound.nemos.backpacks.datagen.lang.UkranianLanguageProvider;
 import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
 import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
 
@@ -15,6 +16,7 @@ public class DataGenerator implements DataGeneratorEntrypoint {
 
         pack.addProvider(ModelProvider::new);
         pack.addProvider(EnglishLanguageProvider::new);
+        pack.addProvider(UkranianLanguageProvider::new);
         pack.addProvider(BackpackRecipeProvider::new);
         pack.addProvider(ItemTagProvider::new);
     }
